be147741b433e3e9345a6a1edc431d119587a96d
Mehdi ElHAIJ <mehdi.elhaij@outlook.com>
2018-01-08 23:07:40 +0000

    bootstrap 4.beta3

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,5 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+    .sass('resources/assets/sass/bootstrap4.scss', 'public/css/bootstrap.css')
+    .sass('resources/assets/sass/app.scss', 'public/css/app.css');
