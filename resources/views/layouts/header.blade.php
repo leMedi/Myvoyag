7bd74ef82293c8e01c30c2d574bceacc2f46239d
Mehdi ElHAIJ <mehdi.elhaij@outlook.com>
2018-01-09 19:49:16 +0000

    jacobs logo

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,5 +1,7 @@
 <nav id="main-header" class="navbar navbar-expand-lg navbar-light">
-	<a class="navbar-brand" href="#">Myvoyag</a>
+	<a class="navbar-brand" href="#">
+		<img src="{{ asset('imgs/logo-jacobs.png') }}" alt="">
+	</a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
 	 aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
