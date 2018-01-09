be7037737530a7ace1adef6d56337c7ab0dc326e
Mehdi ElHAIJ <mehdi.elhaij@outlook.com>
2018-01-09 19:16:32 +0000

    views: header

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,33 @@
+<nav id="main-header" class="navbar navbar-expand-lg navbar-light">
+	<a class="navbar-brand" href="#">Myvoyag</a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+	 aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+
+	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul class="navbar-nav mr-auto">
+			<li class="nav-item active">
+				<a class="nav-link" href="#">Acceuil
+					<span class="sr-only">(current)</span>
+				</a>
+			</li>
+		</ul>
+		<form class="form-inline my-2 my-lg-0">
+			<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+		</form>
+		
+		<div class="dropdown ml-2">
+			<a  href="#" class="dropdown-toggle" role="button" id="dropdwonMenuAvatar" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				<img class="avatar rounded-circle" src="https://zawiastudio.com/dashboard/demo/img/avatar-72.jpg" alt="">
+			</a>
+
+			<div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdwonMenuAvatar">
+				<a class="dropdown-item" href="#">Action</a>
+				<a class="dropdown-item" href="#">Another</a>
+				<a class="dropdown-item" href="#">Something</a>
+			</div>
+		</div>
+
+	</div>
+</nav>
