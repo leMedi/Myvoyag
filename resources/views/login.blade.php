464ac08f71c6d667da1bd9123d5b2a1075843975
achraf elyaacoubi <achraf18el@gmail.com>
2018-01-10 01:28:56 +0000

    view login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,12 +3,18 @@
 
 @section('body')
 
-<div class="container-fluid" >
+<div class="container-fluid" id="login">
 	<div class="row">
-		<div class="col-md-8">
-			
+		<div class="col-md-8 px-0">
+
+			<div class="full-height bg" style= "background-image: url({{ asset('imgs/login.jpg') }})" >
+				<div class="img-caption">
+					<h1 class="caption-title">We make spectacular</h1>
+					<p class="caption-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+				</div>
+			</div>
 		</div>
-		<div class="col-md-4 px-0" id="loginForm">
+		<div class="col-md-4 px-0" >
 			<div class="full-height  bg-white height-100 ">
 				<div class="vertical-align full-height pdd-horizon-70">
 					<div class="table-cell">
@@ -32,12 +38,11 @@
 					</div>				
 				</div>
 				<div class="login-footer">
-					<img class="img-responsive inline-block" src="http://themenate.com/espire/html/dist/assets/images/logo/logo.png" width="100" alt="">
+					<img class="img-responsive inline-block pdd-top-10" src="{{ asset('imgs/logo-jacobs.png') }}" width="100" alt="">
 					<span class="font-size-13 float-right pdd-top-10">Don't have an account? <a href="">Sign Up Now</a></span>
 				</div>				
 			</div>
 		</div>      
 	</div>
 </div>
-
 @endsection
