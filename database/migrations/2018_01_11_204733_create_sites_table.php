4251bb18518780677f1beb6f676964ab03267b03
achraf elyaacoubi <achraf18el@gmail.com>
2018-01-11 21:42:40 +0000

    creating site Table

---
+++ database/migrations/2018_01_11_204733_create_sites_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSitesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sites', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 30);
+            $table->string('country', 30);
+            $table->string('city', 30);
+            $table->string('address')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sites');
+    }
+}
