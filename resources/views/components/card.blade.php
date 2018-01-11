a47aac1164f345a6bdc5beaf0a6f022cdee7c259
Mehdi ElHAIJ <mehdi.elhaij@outlook.com>
2018-01-11 23:16:04 +0000

    Views: card component additionnal classes

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,4 +1,4 @@
-<div class="card mb-4 border {{ (isset($border)) ? 'border-'.$border : '' }}">
+<div class="card mb-4 {{ (isset($class)) ? $class : '' }} border {{ (isset($border)) ? 'border-'.$border : '' }}">
     <div class="card-body">
         {{ $slot }}
     </div>
