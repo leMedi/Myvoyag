52457e601b05c1085c79ab4871bc1796d6c97bc9
achraf elyaacoubi <achraf18el@gmail.com>
2018-01-11 15:34:31 +0000

    modify users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('firstName');
+            $table->string('lastName');
             $table->string('email')->unique();
+            $table->string('departement');
+            $table->enum('type', ['user', 'chef', 'cordi', 'admin']);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
