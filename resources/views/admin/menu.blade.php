f335230e1bc27afd71253b6114c7fd80b135d8a8
Mehdi ElHAIJ <mehdi.elhaij@outlook.com>
2018-01-11 15:40:38 +0000

    views: adlin sidebar menu

---
+++ resources/views/admin/menu.blade.php
@@ -0,0 +1,9 @@
+<aside class="sidebar-1">
+    <h4 class="title">Menu</h4>
+    <ul>
+        <li class="active"><a class="menu-link" href="#">Hotels</a></li>
+        <li><a class="menu-link" href="#">Sites</a></li>
+        <li><a class="menu-link" href="#">Voitures</a></li>
+        <li><a class="menu-link" href="#">Hotels</a></li>
+    </ul>
+</aside>
