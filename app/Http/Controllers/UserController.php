cc486c4d1ae207c8dd7d0c6e3c1c2fcff3966808
Mehdi ElHAIJ <mehdi.elhaij@outlook.com>
2018-01-13 18:29:48 +0000

    Auth midleware

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,6 +12,16 @@ use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function index()
     {
         return view('admin.users', [
