f0ac32881feb708889e11e48940d1fe8c1373c0d
Mehdi ElHAIJ <mehdi.elhaij@outlook.com>
2018-01-13 18:28:24 +0000

    avatar seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,25 +12,26 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'firstname' => 'achraf',
-            'lastname' => 'jacobi',
-            'email' => 'achraf@jacobi.com',
-            'password' => bcrypt('hello'),
-            'departement' => 'info',
-            'type' => 'admin',
-            'numPass' => '12SD48483 1',
-            'tel' => '0699998775'
+            'firstname'     => 'achraf',
+            'lastname'      => 'jacobi',
+            'email'         => 'achraf@jacobi.com',
+            'password'      => bcrypt('hello'),
+            'departement'   => 'info',
+            'type'          => 'admin',
+            'numPass'       => '12SD48483 1',
+            'avatar'        => '1515864362.jpg',
+            'tel'           => '0699998775'
         ]);
 
         DB::table('users')->insert([
-            'firstname' => 'ali',
-            'lastname' => 'jacobi',
-            'email' => 'ali@jacobi.com',
-            'password' => bcrypt('hello'),
-            'departement' => 'info',
-            'type' => 'admin',
-            'numPass' => '89SD47483 1',
-            'tel' => '0675886690'
+            'firstname'     => 'ali',
+            'lastname'      => 'jacobi',
+            'email'         => 'ali@jacobi.com',
+            'password'      => bcrypt('hello'),
+            'departement'   => 'info',
+            'type'          => 'admin',
+            'numPass'       => '89SD47483 1',
+            'tel'           => '0675886690'
         ]);
         
         $this->call(HotelsTableSeeder::class);
