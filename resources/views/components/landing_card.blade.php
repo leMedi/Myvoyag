57ff258b18098c1588fb3a8c62464a0776ead03c
achraf elyaacoubi <achraf18el@gmail.com>
2018-01-15 17:59:44 +0000

    add champs at card

--- resources/views/components/landing_card.blade.php
+++ resources/views/components/landing_card.blade.php
@@ -1,4 +1,4 @@
-<div class="card card-landing p-4">
+<div class="card card-landing p-4 {{$border}}">
 	<div class="card-body text-center">
         @if (isset($img))
 		    <img class="card-img" src="{{ $img }}" alt="">
@@ -6,6 +6,6 @@
         <h4 class="card-title">
         {{ $slot }}
         </h4>
-		<a class="btn btn-primary" href="{{ $action_link }}">{{ $action_text }}</a>
+		<a class="btn {{$btn_classe}}" href="{{ $action_link }}">{{ $action_text }}</a>
 	</div>
 </div>
