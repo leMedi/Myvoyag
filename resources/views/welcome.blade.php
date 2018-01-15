fef496b690dfae0b4925c1b6da8f36b30310dacd
achraf elyaacoubi <achraf18el@gmail.com>
2018-01-15 17:44:52 +0000

    Welcome :modify card

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,11 @@
 @section('body')
 
 <div class="container">
-
+    
     <div class="row">
         <div class="col-12">
             <div class="my-5 text-center">
-                <h2 class="mb-3">Hi Jessica! Welcome back to the Dashboard.</h2>
+                <h2 class="mb-3">Hi {{ Auth::user()->lastName }}! Bienvenue dans l' Acceille.</h2>
                 <p class="text-mute h6">
                     Check out your past searches and the content you’ve browsed in.
                     <a href="">View last results</a>
@@ -15,43 +15,42 @@
             </div>
         </div>
     </div> <!-- .row -->
+
     
-    
-    <div class="row">
-        <div class="col-sm-12 col-lg-4 mb-4">
-            @component('components.landing_card', [
-                'action_text'   => 'Start new Campaign',
-                'action_link'   => '#',
-                'img'           => asset('imgs/icon-intro1.svg')
-                ])
-                All Files ready? <br>
-                Start promoting your apps today.
-                @endcomponent
-            </div>
-            
-            <div class="col-sm-12 col-lg-4 mb-4">
+    @if(Auth::user()->tel == '')
+
+        <div class="row">
+            <div class="col-sm-12 offset-sm-4 col-lg-4 mb-4">
                 @component('components.landing_card', [
-                    'action_text'   => 'Start new Campaign',
-                    'action_link'   => '#',
+                    'action_text'   => 'Remplire Infos',
+                    'border'        => 'border border-danger',
+                    'btn_classe'     => 'btn-danger',
+                    'action_link'   => '/profile/edit',
                     'img'           => asset('imgs/icon-intro2.svg')
                     ])
-                    Check your performance. See the results of all your active campaings.
-                    @endcomponent
-                </div>
-                
-                <div class="col-sm-12 col-lg-4 mb-4">
-                    @component('components.landing_card', [
-                        'action_text'   => 'Start using Dashboard',
-                        'action_link'   => '#',
-                        'img'           => asset('imgs/icon-intro3.svg')
-                        ])
-                Start console and prepare new stuff <br>
-                for your customers or community!
+                    Veuillez Completer Vos informartions de Profile, Merci!
+                @endcomponent
+            </div>
+        </div>
+
+    @else 
+
+        <div class="row">
+            <div class="col-sm-12 col-lg-4 mb-4">
+                @component('components.landing_card', [
+                    'action_text'   => 'Start using Dashboard',
+                    'border'        => '',
+                    'btn_classe'     => 'btn-primary',
+                    'action_link'   => '#',
+                    'img'           => asset('imgs/icon-intro3.svg')
+                    ])
+                    Start console and prepare new stuff <br>
+                    for your customers or community!
                 @endcomponent
             </div>
         </div> <!-- .row -->
 
-    </div>
+</div>
 
     <div class="container-fluid">
         <div class="row">
@@ -94,6 +93,7 @@
             </div>
             <div class="col-lg-2"></div>
         </div>
+        @endif
     </div>
     
 @endsection
