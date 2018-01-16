6aa299044c79349d4f8f59ec2b59021c1576b981
mehdi-elhaij <mehdi.elhaij@hotmail.com>
2018-01-16 17:47:31 +0000

    site hotel relationship

--- app/Site.php
+++ app/Site.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Site extends Model
 {
     public $timestamps = false;
+
+    public function hotels()
+    {
+        return $this->hasMany('App\Hotel');
+    }
 }
